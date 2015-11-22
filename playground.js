c1daf78e20cf5c3f30aaa309891689513289321f
Henrik Korsgaard <korsgaard@cs.au.dk>
2015-11-22 09:50:36 -0700

    test

--- playground.js
+++ playground.js
@@ -1,1 +1,10 @@
-//THIS IS FOR PLAYING ON THE PI
+(function(){
+    'use strict'
+    let exec = require('child_process').exec;
+    let cmd = 'iw wlan0 interface add mon0 type monitor && ip link set mon0 up';
+    exec(cmd, function(err, stdout, stderr){
+        console.log(err);
+        console.log(stdout);
+        console.log(stderr);
+    });
+}());
