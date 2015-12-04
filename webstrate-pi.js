2ca17005651995c35e41e02a3e674a01aaa8a7d3
Henrik Korsgaard <korsgaard@cs.au.dk>
2015-12-03 18:28:44 -0700

    revised proximity scanner and prepared it for testing on PI

--- webstrate-pi.js
+++ webstrate-pi.js
@@ -1,103 +1,108 @@
 /*global console, process, require*/
 ( function () {
-        //SHOULD HANDLE FATAL DB ERRORS HERE! -> try a connection, then close!
-        'use strict';
-        let fs = require( 'fs' );
-        let spawn = require( 'child_process' ).spawn;
-		let pi = require('./lib/models/pi.js');
+    //SHOULD HANDLE FATAL DB ERRORS HERE! -> try a connection, then close!
+    'use strict';
+    let fs = require( 'fs' );
+    let spawn = require( 'child_process' ).spawn;
+    let pi = require( './lib/models/pi.js' );
 
-        let config, phantomConfig, serverConfig;
-        let server, scanner, phantomjs;
+    let config, phantomConfig, serverConfig, scannerConfig;
+    let server, scanner, phantomjs;
 
-		fs.readFile('webstrate-pi-local-configuration.conf', 'utf8', function (err, data) {
-			try{
-				if (err) throw err;
-				config = JSON.parse(data);
-			} catch(e){
-				console.error(err);
-				process.exit(1);
-			}
-			if(config){
-				phantomConfig = {};
-				phantomConfig['webstrate_server'] = config['webstrate_server'];
-				phantomConfig['webstrate_login'] = config['webstrate_login'];
-				phantomConfig['webstrate_password'] = config['webstrate_password'];
-				phantomConfig['webstrate'] = config['webstrate'];
-				phantomConfig['ip'] = config['ip'];
-				
-				serverConfig = {};
-				serverConfig['ip'] = config['ip'];
-				serverConfig['port'] = 1337;//port should be an option on the config.
-				serverConfig['webstrate_server'] = config['webstrate_server'];
-				/*
-				initiatiePhantomJS();
-				updatePIInfo();
-				initiateServer();
-				*/
-				
-			}
-		});
-		
+    fs.readFile( 'webstrate-pi-local-configuration.conf', 'utf8', function ( err, data ) {
+        try {
+            if ( err ) { throw err; }
+            config = JSON.parse( data );
+        } catch ( e ) {
+            console.error( err );
+            process.exit( 1 );
+        }
+        if ( config ) {
+            updatePIInfo();
 
-        function initiateServer() {
-          server = require('./lib/server.js');
+            phantomConfig = {};
+            phantomConfig[ 'webstrate_server' ] = config[ 'webstrate_server' ];
+            phantomConfig[ 'webstrate_login' ] = config[ 'webstrate_login' ];
+            phantomConfig[ 'webstrate_password' ] = config[ 'webstrate_password' ];
+            phantomConfig[ 'webstrate' ] = config[ 'webstrate' ];
+            phantomConfig[ 'ip' ] = config[ 'ip' ];
 
-          server.on('change', function(e){
-              console.log(e);
-          });
+            serverConfig = {};
+            serverConfig[ 'ip' ] = config[ 'ip' ];
+            serverConfig[ 'port' ] = 1337; //port should be an option on the config.
+            serverConfig[ 'webstrate_server' ] = config[ 'webstrate_server' ];
 
-          server.on('error', function(err){
-              console.log(err);
-          });
+            scannerConfig = {};
+            
+            /*
+            initiatiePhantomJS();
 
-          server.listen(serverConfig);
+            initiateServer();
+            */
 
         }
+    } );
 
-		function updatePIInfo(){
-			let piObj = {
-		        mac:config.mac,
-		        ip: config.ip,
-		        stationMac: config.station_mac,
-		        stationIP: config.station_ip,
-		        SSID: config.ssid,
-		        os: config.os,
-				cpu: config.cpu,
-				peripherals:config.peripherals
-		    };
-			
-			pi.upsertPI(piObj, function(err, result){
-				if(err){
-					console.error(err);
-				}
-				console.log(result);
-			})
-		}
 
-        function initiateProximityScanner() {
+    function initiateServer() {
+        server = require( './lib/server.js' );
 
-        }
+        server.on( 'change', function ( e ) {
+            console.log( e );
+        } );
 
-        function initiatiePhantomJS() {
-            phantomjs = spawn( 'phantomjs', [ '--web-security=no', '/scripts/phantom-pi-ws.js', JSON.stringify( phantomConfig ) ] );
+        server.on( 'error', function ( err ) {
+            console.log( err );
+        } );
 
-            phantomjs.stdout.on( 'data', function ( data ) {
-                let msg = data.toString();
-                console.log( msg );
-            } );
+        server.listen( serverConfig );
 
-            phantomjs.stderr.on( 'data', function ( data ) {
-                let err = data.toString();
-                console.log( err );
-            } );
+    }
 
-            phantomjs.on( 'error', function ( err ) {
-                console.log( err );
-            } );
+    function updatePIInfo() {
+        let piObj = {
+            mac: config.mac,
+            ip: config.ip,
+            stationMac: config.station_mac,
+            stationIP: config.station_ip,
+            SSID: config.ssid,
+            os: config.os,
+            cpu: config.cpu,
+            peripherals: config.peripherals
+        };
 
-            phantomjs.on( 'close', function ( code ) {
-                console.log( "closed with code " + code );
-            } );
-        }
+        pi.upsertPI( piObj, function ( err, result ) {
+            if ( err ) {
+                console.error( err );
+            }
+            console.log( result );
+        } );
+    }
 
-}());
+    function initiateProximityScanner() {
+
+    }
+
+    function initiatiePhantomJS() {
+        phantomjs = spawn( 'phantomjs', [ '--web-security=no', '/scripts/phantom-pi-ws.js', JSON.stringify( phantomConfig ) ] );
+
+        phantomjs.stdout.on( 'data', function ( data ) {
+            let msg = data.toString();
+            console.log( msg );
+        } );
+
+        phantomjs.stderr.on( 'data', function ( data ) {
+            let err = data.toString();
+            console.log( err );
+        } );
+
+        phantomjs.on( 'error', function ( err ) {
+            console.log( err );
+        } );
+
+        phantomjs.on( 'close', function ( code ) {
+            console.log( "closed with code " + code );
+        } );
+    }
+
+}() );
