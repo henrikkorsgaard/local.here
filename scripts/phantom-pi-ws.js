6a847fdc33353c98e0bfbdc70dec3616fcf77e85
Henrik Korsgaard <korsgaard@cs.au.dk>
2015-12-05 22:02:59 -0700

    Stuff to test on PI

--- scripts/phantom-pi-ws.js
+++ scripts/phantom-pi-ws.js
@@ -5,9 +5,9 @@ var args = system.args;
 var config;
 try {
     config = JSON.parse( args[ 1 ] );
-    console.log("Setting up webstrate "+config.webstrate_server+"/"+config.webstrate +" on phantomjs with the following PI ip: "+ config.ip );
+    console.log( "Setting up webstrate " + config.webstrate_server + "/" + config.webstrate + " on phantomjs with the following PI ip: " + config.ip );
 } catch ( e ) {
-    console.error('Unable to parse the config parameters from <'+args+'>');
+    console.error( 'Unable to parse the config parameters from <' + args + '>' );
     phantom.exit( 1 );
 }
 page.settings.userName = config.webstrate_login;
@@ -16,7 +16,7 @@ page.settings.password = config.webstrate_password;
 
 page.open( config.webstrate_server + '/' + config.webstrate, function ( status ) {
     if ( status !== 'success' ) {
-        console.error('Unable to connection to the webstrate server <'+config.webstrate_server+'>');
+        console.error( 'Unable to connection to the webstrate server <' + config.webstrate_server + '>' );
         phantom.exit( 1 );
     }
 } );
@@ -24,84 +24,144 @@ page.open( config.webstrate_server + '/' + config.webstrate, function ( status )
 page.onInitialized = function () {
     page.onCallback = function ( data ) {
         if ( data.event === 'loaded' ) {
-            console.log( "Phantom loaded the "+config.webstrate+" webstrate on "+config.webstrate_server);
+            console.log( "Phantom loaded the " + config.webstrate + " webstrate on " + config.webstrate_server );
         } else if ( data.event === 'api loaded' ) {
-            console.log( "Phantom loaded the webstrate api for /"+config.webstrate+" on /"+config.webstrate + "_api");
+            console.log( "Phantom loaded the webstrate api for /" + config.webstrate + " on /" + config.webstrate + "_api" );
         }
     };
 
-    page.evaluate( function (ws, ip) {
+    page.evaluate( function ( ws, ip ) {
         document.addEventListener( 'loaded', function () {
-			var log = document.getElementById(ws+'_console');
-			if(log){
-				log.innerHTML = '';
-				log.style.display = 'none';
-			} else {
-				log = document.createElement('div');
-				log.id = ws+'_console';
-				log.style.display = 'none';
-				document.body.appendChild(log);
-			}
+            var log = document.getElementById( ws + '_console' );
+            if ( log ) {
+                log.innerHTML = '';
+                log.style.display = 'none';
+            } else {
+                log = document.createElement( 'div' );
+                log.id = ws + '_console';
+                log.style.display = 'none';
+                document.body.appendChild( log );
+            }
             var iframe = document.getElementById( ws + '_api' );
             if ( iframe ) {
                 iframe.setAttribute( 'ip', ip );
                 iframe.addEventListener( 'transcluded', function () {
                     var doc = iframe.contentDocument || iframe.contentWindow.document;
-                    var ipEl = doc.getElementById('ip');
-                    if(ipEl){
-                      ipEl.innerHTML = ip;
+                    var ipEl = doc.getElementById( 'pi-ip' );
+                    if ( ipEl ) {
+                        ipEl.innerHTML = ip;
                     } else {
-                      ipEl = doc.createElement('div');
-					  ipEl.id = 'ip';
-                      ipEl.innerHTML = ip;
-                      doc.body.appendChild(ipEl);
+                        ipEl = doc.createElement( 'div' );
+                        ipEl.id = 'pi-ip';
+                        ipEl.className = 'api-essentiels';
+                        ipEl.innerHTML = ip;
+                        doc.body.appendChild( ipEl );
                     }
+
+                    var target = doc.getElementById( 'pi-events' );
+
+                    if ( !target ) {
+                        target = doc.createElement( 'div' );
+                        target.id = 'pi-events';
+                        target.className = 'api-essentiels';
+                        doc.body.appendChild( target );
+                    }
+
+                    // create an observer instance
+                    var observer = new MutationObserver( function ( mutations ) {
+                        if ( mutations[ 0 ].type === 'childList' && mutations[ 0 ].addedNodes.length > 0 && mutations[ 0 ].addedNodes[ 0 ].tagName === 'PING' ) {
+                            console.log( "Got pinged from " + ws + "_api" );
+                            mutations[ 0 ].addedNodes[ 0 ].parentNode.removeChild( mutations[ 0 ].addedNodes[ 0 ] );
+                            target.innerHTML += '<pong></pong>';
+                        }
+                    } );
+
+                    var config = {
+                        attributes: false,
+                        childList: true,
+                        characterData: true
+                    };
+
+                    // pass in the target node, as well as the observer options
+                    observer.observe( target, config );
+
                     window.callPhantom( {
                         "event": "api loaded"
                     } );
                 }, false );
             } else {
-              iframe = document.createElement( 'iframe' );
-              iframe.src = '/' + ws + '_api';
-              iframe.className = 'basic-pi-api';
-              iframe.id = ws + '_api'
-              iframe.seamless = true;
-              iframe.style.display = 'none';
-              iframe.setAttribute( 'ip', ip );
-              document.body.appendChild( iframe );
-              iframe.addEventListener( 'transcluded', function () {
-                var doc = iframe.contentDocument || iframe.contentWindow.document;
-                var ipEl = doc.getElementById('ip');
-                if(ipEl){
-                  ipEl.innerHTML = ip;
-                } else {
-                  ipEl = doc.createElement('div');
-                  ipEl.id = 'ip';
-                  ipEl.innerHTML = ip;
-                  doc.body.appendChild(ipEl);
-                }
-              }, false );
-              window.callPhantom( {
-                  "event": "api loaded"
-              } );
+                iframe = document.createElement( 'iframe' );
+                iframe.src = '/' + ws + '_api';
+                iframe.className = 'basic-pi-api';
+                iframe.id = ws + '_api'
+                iframe.seamless = true;
+                iframe.style.display = 'none';
+                iframe.setAttribute( 'ip', ip );
+                document.body.appendChild( iframe );
+                iframe.addEventListener( 'transcluded', function () {
+                    var doc = iframe.contentDocument || iframe.contentWindow.document;
+                    var ipEl = doc.getElementById( 'pi-ip' );
+                    if ( ipEl ) {
+                        ipEl.innerHTML = ip;
+                    } else {
+                        ipEl = doc.createElement( 'div' );
+                        ipEl.id = 'pi-ip';
+                        ipEl.className = 'api-essentiels';
+                        ipEl.innerHTML = ip;
+                        doc.body.appendChild( ipEl );
+                    }
+
+                    var target = doc.getElementById( 'pi-events' );
+
+                    if ( !target ) {
+                        target = doc.createElement( 'div' );
+                        target.id = 'pi-events';
+                        target.className = 'api-essentiels';
+                        doc.body.appendChild( target );
+                    }
+
+                    // create an observer instance
+                    var observer = new MutationObserver( function ( mutations ) {
+                        if ( mutations[ 0 ].type === 'childList' && mutations[ 0 ].addedNodes.length > 0 && mutations[ 0 ].addedNodes[ 0 ].tagName === 'PING' ) {
+                            console.log( "Got pinged from " + ws + "_api" );
+                            mutations[ 0 ].addedNodes[ 0 ].parentNode.removeChild( mutations[ 0 ].addedNodes[ 0 ] );
+                            target.innerHTML += '<pong></pong>';
+                        }
+                    } );
+
+                    var config = {
+                        attributes: false,
+                        childList: true,
+                        characterData: true
+                    };
+
+                    // pass in the target node, as well as the observer options
+                    observer.observe( target, config );
+
+                    window.callPhantom( {
+                        "event": "api loaded"
+                    } );
+
+                }, false );
+
             }
             window.callPhantom( {
                 "event": "loaded"
             } );
         }, false );
-    }, config.webstrate, config.ip);
+    }, config.webstrate, config.ip );
 };
 
-page.onConsoleMessage = function(msg) {
-	var consoleMsg = 'CONSOLE: ' + JSON.stringify(msg);
-	console.log(consoleMsg);
-	page.evaluate(function(msg, ws){
-		var log = document.getElementById(ws+'_console');
-		log.innerHTML += '<div>'+msg+'</div>';
-	}, consoleMsg, config.webstrate);
+page.onConsoleMessage = function ( msg ) {
+    var consoleMsg = 'CONSOLE: ' + JSON.stringify( msg );
+    console.log( consoleMsg );
+    page.evaluate( function ( msg, ws ) {
+        var log = document.getElementById( ws + '_console' );
+        log.innerHTML += '<div>' + msg + '</div>';
+    }, consoleMsg, config.webstrate );
 };
 
 page.onError = function ( msg, trace ) {
-	console.error("Error on page - unable to render page. Error " + msg);
-	phantom.exit(1);
+    console.error( "Error on page - unable to render page. Error " + msg );
+    phantom.exit( 1 );
 };
